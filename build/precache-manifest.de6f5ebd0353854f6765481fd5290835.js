30e827eb90155049e9516442b579ce06b1a37d76
joshua cowan <joshuacowan87@gmail.com>
2021-10-22 21:15:39 -0700

    add live link

---
+++ build/precache-manifest.de6f5ebd0353854f6765481fd5290835.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4122109fab7c0695483749210a76cebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76c14be0f07cff007ece",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c14be0f07cff007ece",
+    "url": "/static/js/main.a67a7599.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
